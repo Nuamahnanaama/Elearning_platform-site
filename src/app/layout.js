015c3417f835f2cb2e201845911d8c7e1764db46
unknown <dansowaanuamah120@gmail.com>
2024-10-29 11:34:38 +0100

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,12 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import {Outfit} from "next/font/google"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const Outfit_init = Outfit({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: '--font-outfit'
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={Outfit_init.variable}
       >
         {children}
       </body>
